abf1c7ab51cde9831d1df49a350e10ec2ae7d499
hdenni3 <hdenni3@gmail.com>
2024-08-11 14:57:02 -0500

    final

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ds_module_14_belly-button-challenge
 Homework for Module 14 - belly button challenge
+
+This project will display an interactive dashboard to showcase a belly button biodiversity dataset. The dashboard will display data when a subject ID number is selected and give the corresponding demographic information, the top ten cultures found, and the bacteria cultures per sample.
--- Submission_Dennis/static/js/app.js
+++ Submission_Dennis/static/js/app.js
@@ -3,20 +3,22 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let info = metadata.filter(x => x.id == sample)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    for (const [key, value] of Object.entries(info)) {
+      panel.append("h6").text(`${key}: ${value}`);
+    }
   });
 }
 
@@ -125,6 +127,7 @@ function init() {
 
     // Build charts and metadata panel with the first sample
     buildCharts(first_sample);
+    buildMetadata(default_name);
   });
 }
 
@@ -132,6 +135,7 @@ function init() {
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
   buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
